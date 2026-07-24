4146f15ccfc8c051ed96c0976fc8efcc04affe92
beomkjoon <tjrex@naver.com>
2026-07-24 14:29:16 +0900

    kinolights_크롤링

--- review_analysis/crawling/main.py
+++ review_analysis/crawling/main.py
@@ -3,11 +3,13 @@ from typing import Dict, Type
 from review_analysis.crawling.base_crawler import BaseCrawler
 from review_analysis.crawling.example_crawler import ExampleCrawler
 from review_analysis.crawling.megabox_crawler import MegaboxCrawler
+from review_analysis.crawling.kinolights_crawler import KinolightsCrawler
 
 # 모든 크롤링 클래스를 예시 형식으로 적어주세요. 
 CRAWLER_CLASSES: Dict[str, Type[BaseCrawler]] = {
     "example": ExampleCrawler,
     "megabox": MegaboxCrawler,
+    "kinolights": KinolightsCrawler,
 }
 
 def create_parser() -> ArgumentParser:
