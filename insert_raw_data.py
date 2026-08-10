e35355521ce7eac302d644d1663e2d4c6046dc85
나예린 <erin3548@yonsei.ac.kr>
2026-08-10 16:56:29 +0900

    AWS 작업 (#10)
    * AWS 작업
    
    * Remove hardcoded MongoDB credentials, use env var
    
    * Add *.pem to gitignore

--- insert_raw_data.py
+++ insert_raw_data.py
@@ -1,7 +1,11 @@
+import os
 import pandas as pd
 from pymongo import MongoClient
+from dotenv import load_dotenv
 
-client = MongoClient("mongodb://localhost:27017")
+load_dotenv()
+
+client = MongoClient(os.getenv("MONGO_URL"))
 db = client["ybigta_db"]
 
 # site_name : CSV 파일 경로
